65b88c7227843a9bd8fed09bc6c7566fe9145ac8
prasthant <adhibro999@gmail.com>
2025-01-16 07:58:57 +0545

    Red Jacket

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { crosswordData } from '../data/crosswordData';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold mb-8">BIT IIIB WEB TECH</h1>
+      <h1 className="text-4xl font-bold mb-8">Easter Tamang</h1>
       <Crossword data={crosswordData} />
     </main>
   );
